4764c964e1dfb3995a1d030281f25a15868e7934
doctordanasmith <doc.danasmith@gmail.com>
2023-04-11 18:08:46 -0300

    2

--- 25 DE MAYO consultas.R
+++ 25 DE MAYO consultas.R
@@ -5,27 +5,10 @@ library(lubridate)
 
 library(readxl)
 
-Consultas_codificadas_primeros_del_2022 <- read_csv2("Consultas_codificadas.csv")  %>% 
-  rename(Codigo = `Hash MD5`) %>% 
-  mutate(`FECHA RECEP` = dmy(Fecha), Localidad = str_to_upper(Localidad)) %>% 
-  mutate (AÑO = case_when(str_detect(string = `FECHA RECEP`, pattern = "^2018") ~ "2018", 
-                          str_detect(string = `FECHA RECEP`, pattern = "^2019") ~ "2019",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2020") ~ "2020",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2021") ~ "2021",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2022") ~ "2022")) %>% 
-  filter(AÑO == "2022")
-
-Consultas_resto_2022 <- read_excel("consultas a partir de Sept 2022 25 de Mayo.xlsx")%>% 
-  mutate(`FECHA RECEP` = dmy(`Fecha/hora atención`), Localidad = str_to_upper(Localidad)) %>% 
-  mutate (AÑO = case_when(str_detect(string = `FECHA RECEP`, pattern = "^2018") ~ "2018", 
-                          str_detect(string = `FECHA RECEP`, pattern = "^2019") ~ "2019",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2020") ~ "2020",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2021") ~ "2021",
-                          str_detect(string = `FECHA RECEP`, pattern = "^2022") ~ "2022"))
-
-
-Consultas_2022 <- Consultas_codificadas_primeros_del_2022 %>% 
-  bind_rows(Consultas_resto_2022)
+Consultas_codificadas <- read_excel("consultas a partir de Sept 2022 25 de Mayo.xlsx") %>% 
+  mutate(`FECHA RECEP` = dmy(`Fecha/hora atención`),
+         Localidad = str_to_upper(Localidad) ) %>% 
+  rename(DIAGNOSTICO = Consulta)
 
 x <- c("MAR DEL PLATA", "LAGUNA DE LOS PADRES", "SAN FRANCISCO", "LOS ORTIZ", "LOMA ALTA", "CHAPADMALAL",
        "EL TEJADO", "SANTA ISABEL", "ESTACION CAMET", "ESTACION CHAPADMALAL", "BATAN",
@@ -33,7 +16,7 @@ x <- c("MAR DEL PLATA", "LAGUNA DE LOS PADRES", "SAN FRANCISCO", "LOS ORTIZ", "L
        "GLORIA DE LA PEREGRINA", "EL DORADO", "LAS MARGARITAS", "2 DE ABRIL", 
        "LA ADELA", "EL SOSIEGO", "LAS QUINTAS", "LOS ACANTILADOS", "SAN EDUARDO", "SIERRA DE LOS PADRES")
 
-POSITIVOS <- Consultas_2022 %>% 
+POSITIVOS <- Consultas_codificadas %>% 
   mutate(CANCER = case_when(
     str_detect(string = DIAGNOSTICO, pattern = "CARCINOMA") ~ "SI",
     str_detect(string = DIAGNOSTICO, pattern = " CA[. ] ") ~ "SI",
@@ -140,44 +123,75 @@ POSITIVOS <- Consultas_2022 %>%
     str_detect(string = DIAGNOSTICO, pattern = "HISTIOCITOSIS") ~ "SI",
     str_detect(string = DIAGNOSTICO, pattern = "SENO ENDOD[EÉ]RMICO") ~ "SI",
     str_detect(string = DIAGNOSTICO, pattern = "TUMOR RABDOIDE") ~ "SI",
-    str_detect(string = DIAGNOSTICO, pattern = "ANEMIA REFRACTARIA") ~ "SI")) %>%  
+    str_detect(string = DIAGNOSTICO, pattern = "ANEMIA REFRACTARIA") ~ "SI")) %>% 
+  mutate (AÑO = case_when(str_detect(string = `FECHA RECEP`, pattern = "^2018") ~ "2018", 
+                          str_detect(string = `FECHA RECEP`, pattern = "^2019") ~ "2019",
+                          str_detect(string = `FECHA RECEP`, pattern = "^2020") ~ "2020",
+                          str_detect(string = `FECHA RECEP`, pattern = "^2021") ~ "2021",
+                          str_detect(string = `FECHA RECEP`, pattern = "^2022") ~ "2022")) %>%  
   filter(CANCER == "SI") %>% 
   filter(Localidad %in% x ) %>% 
   select(Codigo,Sexo, F.Nac., Edad,  `FECHA RECEP` , DIAGNOSTICO , Localidad, AÑO) %>% 
   arrange(`FECHA RECEP`) %>% 
-  arrange(Codigo)
+  distinct(Codigo, .keep_all = T)
 
 
-DNI_y_DNI_codificados_Canreg <- read_excel("2022 - 09 - 16 codigos.xlsx") %>% 
-  rename(Codigo = `Hash MD5`)
+DNI_y_DNI_codificados_Canreg <- read_excel("2022 - 09 - 16 DNI codificados.xlsx") %>% 
+  rename(Codigo = `Hash MD5`) %>% 
+  filter(!is.na(Codigo))
 
-Base_CANREG<- read_csv(".csv")
+Base_canreg <- read_csv("2023 - 04 - 11.csv")
 
-Consultas_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG <- POSITIVOS %>%  
-  semi_join(DNI_y_DNI_codificados_Canreg, by = "Codigo" ) 
+consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG <- POSITIVOS %>%  
+  left_join(DNI_y_DNI_codificados_Canreg, by = "Codigo" ) %>% 
+  filter(!is.na(DNI)) %>% 
+  mutate(ID = row_number()) %>% 
+  select(DNI, Sexo, F.Nac., Edad, `FECHA RECEP`, DIAGNOSTICO, Localidad, AÑO, ID,
+         Codigo)
+
+write_excel_csv2(consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG ,
+                 "consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG.csv ")
 
 TODO_MENOS_DUPLICADOS_DE_DNI_DE_BASE_CANREG <- POSITIVOS %>% 
-  anti_join(Consultas_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG)
+  anti_join(consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG)
 
-Consultas_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG <- 
+consultas_resto_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG <- 
   TODO_MENOS_DUPLICADOS_DE_DNI_DE_BASE_CANREG %>% 
-  semi_join(Base_CANREG, by = "Codigo") 
-
-Consultas_2022_DUPLICADOS <- 
-  Consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG %>% 
-  bind_rows(Consultas_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG) %>% 
+  semi_join(Base_canreg, by = "Codigo") %>% 
   mutate(ID = row_number())
 
-write_excel_csv2(Consultas_resto_2022_DUPLICADOS, 
-                 "Consultas_2022_DUPLICADOS.csv" )
+write_excel_csv2(consultas_resto_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG , 
+                 "consultas_resto_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG.csv" )
 
-INTERNACIONES_2022_UNICOS <- POSITIVOS %>%  
-  anti_join(consultas_2022_DUPLICADOS, by = "Codigo" ) %>% 
+consultas_resto_2022_UNICOS <- POSITIVOS %>%  
+  anti_join(consultas_resto_2022_DUPLICADOS_CON_Codigos_DE_Base_CANREG, by = "Codigo" ) %>% 
+  anti_join(consultas_resto_2022_DUPLICADOS_CON_DNI_DE_Base_CANREG, by ="Codigo")
+
+
+CLAVE_DUPLICADOS <- consultas_resto_2022_UNICOS %>% 
+  count(Codigo, sort = T) %>% 
+  filter(n > 1) 
+
+UNICOS_resto2022_dentrodemismabase <- consultas_resto_2022_UNICOS %>% 
+  semi_join(CLAVE_DUPLICADOS)%>% 
   arrange(Codigo) %>% 
   mutate(ID = row_number())
 
-write_excel_csv2(Consultas_2022_UNICOS, 
-                 "Consultas_2022_UNICOS.csv")  
+write_excel_csv2(UNICOS_resto2022_dentrodemismabase, 
+                 "consultasUNICOS_resto2022_DUPLICADOSdentrodemismabase.csv")
+
+CLAVE_NODUPLICADOS <- consultas_resto_2022_UNICOS %>% 
+  count(Codigo, sort = T) %>% 
+  filter(n == 1)  
+
+UNICOS_resto2022_solounavezenUNICOS <- consultas_resto_2022_UNICOS %>% 
+  semi_join(CLAVE_NODUPLICADOS)%>% 
+  arrange(Codigo) %>% 
+  mutate(ID = row_number())
+
+write_excel_csv2(UNICOS_resto2022_solounavezenUNICOS, 
+                 "consultasUNICOS_resto2022_solounavezenUNICOS.csv")  
+
 
 
 
